0633ac164fe073f8496a40b087d7b30f43023207
unknown <jitesh0dubey@gmail.com>
2020-08-17 18:41:52 +0530

    [Jitesh Dubey] ADDED:Initial Commit

---
+++ CabInvoiceGeneratorTest/CabInvoiceTest.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="CabInvoiceTest.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Tests
+{
+    using NUnit.Framework;
+
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
